4d8b86f50398454c47e545cc8b31961a3fd9b03b
neinafua <achintimorales@colegioguanenta.edu.co>
2024-02-09 07:02:59 -0500

    se realizo analisis y diseño

---
+++ area_perimetro_circulo.py
@@ -0,0 +1,12 @@
+import math
+
+# imput
+r = imput("ingrese el radio: ")
+
+# processing
+a = math.pi * r**2
+p = 2 * math.pi * r
+
+#output
+print("El área es: " + str(a))
+print("El perímetro es: " + str(p))
